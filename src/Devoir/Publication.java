99e479f5929af265490ea61e8c8da91fe11e4de5
Finoana <hei.finoana.6@gmail.com>
2025-08-07 15:04:41 +0300

    ajout

--- src/Devoir/Publication.java
+++ src/Devoir/Publication.java
@@ -1,5 +1,28 @@
 package Devoir;
 
 public class Publication {
-    
+    private final User author;
+    private final String content;
+    private final LocalDateTime createdAt;
+    private final List<Comment> comments = new ArrayList<>();
+
+    public Post(User author, String content) {
+        this.author = author;
+        this.content = content;
+        this.createdAt = LocalDateTime.now();
+
+    public void addComment(Comment comment) {
+        comments.add(comment);
+    }
+
+    public void removeComment(Comment comment) {
+        comments.remove(comment);
+    }
+
+    public User getAuthor() { return author; }
+    public String getContent() { return content; }
+    public List<Comment> getComments() { return comments; }
 }
+
+}
+
