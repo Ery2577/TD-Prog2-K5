fd16da7eab45f73a74cafee4f9f4d3b2870d50e9
Heriniaina Ramaherito <heriniainaramaherito@gmail.com>
2025-08-08 11:18:20 +0300

    Ajout de tout les fonctions sur la classe GroupUtilisateur

--- src/Devoir/code/GroupUtilisateur.java
+++ src/Devoir/code/GroupUtilisateur.java
@@ -15,12 +15,43 @@ public class GroupUtilisateur {
         membre.add(admin);
     }
 
-    public void ajouterCommentaire(Utilisateur){
-        membre.add(Utilisateur);
+     public void ajouterMembre(Utilisateur user){
+        membre.add(user);
     }
 
-    public boolean contientUtilisateur()
+    public boolean contientUtilisateur(Utilisateur user){
+        return membre.contains(user);
+    }
 
-    
+    public void ajouterPublication(Publication pub){
+        publications.add(pub);
+    }
+
+    public void supprimerPublication(Publication pub, Utilisateur user){
+        if (user instanceof Moderateur){
+            publications.remove(pub);
+        }
+    }
+
+    public List<Publication> chercherParMotCle(String motCle){
+        List<Publication> resultats = new ArrayList<>();
+        for (Publication pub : publications){
+            if (pub.getContenu().toLowerCase().contains(motCle.toLowerCase())){
+                resultats.add(pub);
+            }
+        }
+        return resultats;
+    }
+
+
+    public int getNombrePublication(){
+        return publications.size();
+    }
+
+    public void supprimerUtilisateur (Utilisateur cible, Utilisateur demandeur){
+        if (demandeur instanceof Administrateur){
+            membre.remove(cible);
+        }
+    }
 
 }
