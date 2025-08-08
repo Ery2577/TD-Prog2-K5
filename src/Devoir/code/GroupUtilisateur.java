1209024630472c9e0660989e0fdb9b8f93f09ec1
Heriniaina Ramaherito <heriniainaramaherito@gmail.com>
2025-08-08 10:23:17 +0300

    ...

--- src/Devoir/code/GroupUtilisateur.java
+++ src/Devoir/code/GroupUtilisateur.java
@@ -1,9 +1,26 @@
 package Devoir.code;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GroupUtilisateur {
     private List<Utilisateur> membre;
     private List<Publication> publications;
     private Administrateur administrateur;
 
+    public GroupUtilisateur(Administrateur admin){
+        this.administrateur = admin;
+        this.membre = new ArrayList<>();
+        this.publications = new ArrayList<>();
+        membre.add(admin);
+    }
+
+    public void ajouterCommentaire(Utilisateur){
+        membre.add(Utilisateur);
+    }
+
+    public boolean contientUtilisateur()
+
+    
 
 }
